bee60d924e5a399bdb06525b18f5b9eeb0ef1ede
ClayTMS <bboyClay89@gmail.com>
2022-11-21 18:17:08 +0300

    ДЗ 17 (2 задание) (20%)

--- src/ScanDoc.java
+++ src/ScanDoc.java
@@ -17,15 +17,23 @@ public class ScanDoc {
 
         list.addAll(list1);
         String text = list.toString();
+        String textOut = "";
         System.out.println(text);
-        String regexText = "\\+\\d{3}\\W\\d{2}\\W\\d{7}";
+        String phone = "(\\+\\d{3}\\W\\d{2}\\W\\d{7})";
+        String docName = "((\\d{4}\\-[a-zA-Zа-яА-Я]{3}\\-){2}((\\d[a-zA-Zа-яА-Я]){2}))";
+        String email = "([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6})"; //bboyClay89@gmail.com
+        String regexText = phone + "|" + docName + "|" + email;
         Pattern pattern = Pattern.compile(regexText);
         Matcher matcher = pattern.matcher(text);
 
-        if (matcher.find()) {
-            System.out.println(text.substring(matcher.start(), matcher.end()));
-        } else {
-            System.out.println("нет телефона");
+
+        while (matcher.find()) {
+            textOut += text.substring(matcher.start(), matcher.end()) + "\n";
+
+//            System.out.println(text.substring(matcher.start(), matcher.end()));
+
         }
+        System.out.println(textOut);
+
     }
 }
